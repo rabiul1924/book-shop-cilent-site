b5d280c17bcc2f146756e3cc5d75a66e704fe129
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-06-05 16:54:11 +0600

    all okay

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Event = ({event}) => {
     return (
-        <div>
-            <h1>{event.name}</h1>
-            <h1>{event.age}</h1>
-            <h1>{event.description}</h1>
+        <div className="col-3">
+            <Card>
+              <Card.Header>Name: {event.name}</Card.Header>
+      <Card.Body>
+      <Card.Title> Personal Data </Card.Title>
+      <Card.Text>
+      <h1>Your age: {event.age}</h1> 
+      <br />
+      <h1>Description: {event.description}</h1>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+         
+            
+            
         </div>
     );
 };
